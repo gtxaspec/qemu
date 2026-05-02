35497995c70adae27fc0555b170144be0c49069c
gtxaspec <gtxent@gmail.com>
2026-05-02 07:27:20 -0700

    hw/misc/ingenic: add T31 SPI Flash Controller (SFC)
    Add SFC model with PIO (CPU mode) read support, backed by a flash
    image from the command line via -drive if=none.
    
    The SFC implements the register interface used by the T31 SPL SFC NOR
    driver: GLB, DEV_CONF, TRAN_CONF, TRAN_LEN, DEV_ADDR, TRIG, SR, SCR,
    and the DR data FIFO register at offset 0x1000. Transfers are triggered
    by writing START to TRIG, with data delivered in 31-word FIFO chunks.
    
    Supports SPI NOR commands: READ (0x03), FAST_READ (0x0B), READ_ID
    (0x9F), and READ_STATUS (0x05). Reports as Winbond W25Q128 (16 MiB).
    
    With this change, the T31 SPL successfully loads U-Boot from a flash
    image, decompresses it, and U-Boot boots to the environment init stage.
    
    Example:
      qemu-system-mipsel -M ingenic-t31 -m 128M \
        -kernel spl/u-boot-spl \
        -drive file=u-boot-lzo-with-spl.bin,format=raw,if=none,readonly=on \
        -serial null -serial stdio -display none
    
    Signed-off-by: Alfonso Gamboa <gtxent@gmail.com>

---
+++ include/hw/misc/ingenic-t31-sfc.h
@@ -0,0 +1,50 @@
+/*
+ * Ingenic T31 SPI Flash Controller (SFC) emulation
+ *
+ * Copyright (C) 2026 Alfonso Gamboa <gtxent@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MISC_INGENIC_T31_SFC_H
+#define HW_MISC_INGENIC_T31_SFC_H
+
+#include "hw/core/sysbus.h"
+#include "qom/object.h"
+
+#define TYPE_INGENIC_T31_SFC "ingenic-t31-sfc"
+OBJECT_DECLARE_SIMPLE_TYPE(IngenicT31SfcState, INGENIC_T31_SFC)
+
+#define INGENIC_T31_SFC_FLASH_SIZE  (16 * 1024 * 1024)
+#define INGENIC_T31_SFC_FIFO_DEPTH  64
+
+struct IngenicT31SfcState {
+    /*< private >*/
+    SysBusDevice parent_obj;
+    /*< public >*/
+
+    MemoryRegion iomem;
+
+    uint32_t glb;
+    uint32_t dev_conf;
+    uint32_t tran_conf[6];
+    uint32_t tran_len;
+    uint32_t dev_addr[6];
+    uint32_t dev_addr_plus[6];
+    uint32_t mem_addr;
+    uint32_t sr;
+    uint32_t scr;
+    uint32_t intc;
+    uint32_t cge;
+
+    uint32_t fifo[INGENIC_T31_SFC_FIFO_DEPTH];
+    uint32_t fifo_pos;
+    uint32_t fifo_len;
+    uint32_t flash_pos;
+    uint32_t words_total;
+
+    uint8_t *flash_data;
+    uint32_t flash_size;
+};
+
+#endif /* HW_MISC_INGENIC_T31_SFC_H */
