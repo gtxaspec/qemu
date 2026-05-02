24b232291e6de6ab64ef338b417da30791d86210
gtxaspec <gtxent@gmail.com>
2026-05-02 06:07:38 -0700

    hw/mips/ingenic: add T31 SoC skeleton and board definition
    Add initial Ingenic T31 SoC emulation targeting the XBurst1 MIPS32r2
    processor used in IP cameras and IoT devices.
    
    The SoC skeleton includes:
    - XBurstR2 CPU (MIPS32r2 + MXU)
    - TCSM (16 KiB) and SRAM (4 KiB) mapped as RAM
    - 3 UARTs via serial_mm (16550 compatible)
    - 42 peripherals stubbed via create_unimplemented_device()
    - Complete physical memory map from vendor U-Boot headers
    
    The board definition provides:
    - Machine type "ingenic-t31" with 64 MiB default SDRAM
    - Firmware loading via -kernel into TCSM
    - ELF and raw binary support
    
    Signed-off-by: Alfonso Gamboa <gtxent@gmail.com>

---
+++ include/hw/mips/ingenic-t31.h
@@ -0,0 +1,104 @@
+/*
+ * Ingenic T31 System on Chip emulation
+ *
+ * Copyright (C) 2026 Alfonso Gamboa <gtxent@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MIPS_INGENIC_T31_H
+#define HW_MIPS_INGENIC_T31_H
+
+#include "qom/object.h"
+#include "hw/mips/mips.h"
+
+/**
+ * Ingenic T31 device list
+ *
+ * This enumeration is used to refer to a particular device in the
+ * Ingenic T31 SoC. The physical memory base address for each device
+ * can be found in the ingenic_t31_memmap array using the device enum
+ * value as index.
+ */
+enum {
+    /* APB bus */
+    INGENIC_T31_DEV_CPM,
+    INGENIC_T31_DEV_INTC,
+    INGENIC_T31_DEV_TCU,
+    INGENIC_T31_DEV_RTC,
+    INGENIC_T31_DEV_GPIO,
+    INGENIC_T31_DEV_AIC,
+    INGENIC_T31_DEV_CODEC,
+    INGENIC_T31_DEV_UART0,
+    INGENIC_T31_DEV_UART1,
+    INGENIC_T31_DEV_UART2,
+    INGENIC_T31_DEV_DMIC,
+    INGENIC_T31_DEV_SSI0,
+    INGENIC_T31_DEV_SSI1,
+    INGENIC_T31_DEV_I2C0,
+    INGENIC_T31_DEV_I2C1,
+    INGENIC_T31_DEV_USBPHY,
+    INGENIC_T31_DEV_DES,
+    INGENIC_T31_DEV_SADC,
+    INGENIC_T31_DEV_DTRNG,
+
+    /* Standalone */
+    INGENIC_T31_DEV_OST,
+
+    /* AHB0 bus */
+    INGENIC_T31_DEV_HARB0,
+    INGENIC_T31_DEV_DDR_PHY,
+    INGENIC_T31_DEV_LCDC,
+    INGENIC_T31_DEV_IPU,
+    INGENIC_T31_DEV_DDRC,
+
+    /* AHB1 bus (video) */
+    INGENIC_T31_DEV_SCH,
+    INGENIC_T31_DEV_VDMA,
+    INGENIC_T31_DEV_EFE,
+    INGENIC_T31_DEV_MCE,
+    INGENIC_T31_DEV_DBLK,
+    INGENIC_T31_DEV_VMAU,
+    INGENIC_T31_DEV_SDE,
+    INGENIC_T31_DEV_AUX,
+    INGENIC_T31_DEV_TCSM,
+    INGENIC_T31_DEV_JPGC,
+    INGENIC_T31_DEV_SRAM,
+
+    /* AHB2 bus */
+    INGENIC_T31_DEV_HARB2,
+    INGENIC_T31_DEV_NEMC,
+    INGENIC_T31_DEV_PDMA,
+    INGENIC_T31_DEV_AES,
+    INGENIC_T31_DEV_SFC,
+    INGENIC_T31_DEV_MSC0,
+    INGENIC_T31_DEV_MSC1,
+    INGENIC_T31_DEV_HASH,
+    INGENIC_T31_DEV_GMAC,
+    INGENIC_T31_DEV_OTG,
+    INGENIC_T31_DEV_EFUSE,
+
+    /* Memory */
+    INGENIC_T31_DEV_SDRAM,
+};
+
+#define INGENIC_T31_TCSM_SIZE       (16 * 1024)
+#define INGENIC_T31_SRAM_SIZE       (4 * 1024)
+
+#define TYPE_INGENIC_T31 "ingenic-t31"
+OBJECT_DECLARE_SIMPLE_TYPE(IngenicT31State, INGENIC_T31)
+
+struct IngenicT31State {
+    /*< private >*/
+    DeviceState parent_obj;
+    /*< public >*/
+
+    const hwaddr *memmap;
+
+    MemoryRegion tcsm;
+    MemoryRegion sram;
+};
+
+extern const hwaddr ingenic_t31_memmap[];
+
+#endif /* HW_MIPS_INGENIC_T31_H */
