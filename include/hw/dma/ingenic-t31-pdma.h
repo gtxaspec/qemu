3694700c1a7ae29bedc1f8a67619eb42cdb450d9
gtxaspec <gtxent@gmail.com>
2026-05-09 02:02:40 -0700

    hw/dma: add Ingenic T31 PDMA controller
    32-channel programmable DMA with descriptor chain and single-transfer
    modes. The kernel's jz_dma driver (drivers/dma/jzdma/jz_dma.c) uses
    this for all peripheral DMA: audio AIC/I2S, SPI, UART, I2C, MSC.
    
    Supports:
    - Per-channel registers: DSA/DTA/DTC/DRT/DCS/DCM/DDA/DSD
    - Global IRQ aggregation via DIRQP (W1C)
    - Descriptor chain walking (DCM.LINK) with up to 256 descs
    - Single-transfer mode (DCS.NDES)
    - Transfer completion (DCS.TT) + IRQ (DCM.TIE)
    - TSZ-aware byte count (DCM bits 10:8 select unit size)
    - Wired to INTC source 0 (IRQ_PDMA)
    
    Replaces the unimplemented_device stub at 0x13420000.
    Boot log: 'jz-dma jz-dma: JZ SoC DMA initialized'

---
+++ include/hw/dma/ingenic-t31-pdma.h
@@ -0,0 +1,54 @@
+/*
+ * Ingenic T31 PDMA (Programmable DMA) controller
+ *
+ * Copyright (C) 2026 Alfonso Gamboa <gtxent@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_DMA_INGENIC_T31_PDMA_H
+#define HW_DMA_INGENIC_T31_PDMA_H
+
+#include "hw/core/sysbus.h"
+#include "qom/object.h"
+
+#define TYPE_INGENIC_T31_PDMA "ingenic-t31-pdma"
+OBJECT_DECLARE_SIMPLE_TYPE(IngenicT31PdmaState, INGENIC_T31_PDMA)
+
+#define PDMA_NR_CHANNELS  32
+
+struct IngenicT31PdmaChannel {
+    uint32_t dsa;
+    uint32_t dta;
+    uint32_t dtc;
+    uint32_t drt;
+    uint32_t dcs;
+    uint32_t dcm;
+    uint32_t dda;
+    uint32_t dsd;
+};
+
+struct IngenicT31PdmaState {
+    SysBusDevice parent_obj;
+
+    MemoryRegion iomem;
+    qemu_irq irq;
+
+    struct IngenicT31PdmaChannel ch[PDMA_NR_CHANNELS];
+
+    uint32_t dmac;
+    uint32_t dirqp;
+    uint32_t ddr;
+    uint32_t ddrs;
+    uint32_t dmacp;
+    uint32_t dsirqp;
+    uint32_t dsirqm;
+    uint32_t dcirqp;
+    uint32_t dcirqm;
+    uint32_t dmcs;
+    uint32_t dmnmb;
+    uint32_t dmsmb;
+    uint32_t dmint;
+};
+
+#endif
