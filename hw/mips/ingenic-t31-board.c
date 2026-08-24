312d6e935adc07794bef4d60058188b3f48004d4
gtxaspec <gtxent@gmail.com>
2026-08-24 07:58:26 -0700

    ingenic: raise SPL size limit to 256 KiB
    U-Boot 2026.10 SPL for XBurst2 SoCs is ~88 KiB, exceeding the
    64 KiB sanity check that was based on the older 2013.07 SPL.

--- hw/mips/ingenic-t31-board.c
+++ hw/mips/ingenic-t31-board.c
@@ -321,7 +321,7 @@ static void ingenic_t31_board_init(MachineState *machine)
         }
 
         uint32_t spl_size = ldl_le_p(&header[12]);
-        if (spl_size == 0 || spl_size > 64 * KiB) {
+        if (spl_size == 0 || spl_size > 256 * KiB) {
             error_report("SPL size %u out of range", spl_size);
             exit(1);
         }
